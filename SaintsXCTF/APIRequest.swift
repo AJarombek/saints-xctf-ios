7427526806d6db53dd535154049c32deb0cb4581
Andrew Jarombek <ajarombek95@gmail.com>
2017-06-12 23:00:10 -0400

    V.0.0.11 - Finish API Request Class

--- SaintsXCTF/APIRequest.swift
+++ SaintsXCTF/APIRequest.swift
@@ -71,17 +71,138 @@ class APIRequest {
     }
     
     // Handle HTTP POST Requests to the API
-    public static func post(withURL url: String) {
+    public static func post(withURL url: URL, andObject object: Any,
+                            completion: @escaping (AnyObject) -> Void) {
+        var urlrequest = URLRequest(url: url)
+        urlrequest.httpMethod = "POST"
         
+        // Object to hold the JSON string
+        let jsonObject: Data
+        
+        // Serialize the object into JSON for the URL Request
+        do {
+            jsonObject = try JSONSerialization.data(withJSONObject: object, options: [])
+            urlrequest.httpBody = jsonObject
+        } catch {
+            print("\(logTag) Error, cannot produce JSON from object")
+            return
+        }
+        
+        let task = session.dataTask(with: urlrequest) {
+            (data, response, error) -> Void in
+            
+            do {
+                
+                // Check for any errors
+                guard error == nil else {
+                    print("\(logTag) Error with POST request")
+                    print(error!)
+                    return
+                }
+                
+                // Check the response data
+                guard let responseData = data else {
+                    print("\(logTag) Error, did not receive data")
+                    return
+                }
+                
+                print(responseData)
+                
+                // Serialize with JSON
+                let json: AnyObject? = try JSONSerialization.jsonObject(
+                    with: responseData, options: .mutableContainers) as AnyObject
+                
+                if let j: AnyObject = json {
+                    
+                    OperationQueue.main.addOperation {
+                        completion(j)
+                    }
+                    
+                } else {
+                    print("\(logTag) Error, Map Conversion Failed")
+                    return
+                }
+                
+            } catch let error {
+                print(error)
+            }
+        }
+        task.resume()
     }
     
     // Handle HTTP PUT Requests to the API
-    public static func put(withURL url: String) {
+    public static func put(withURL url: URL, andObject object: Any,
+                           completion: @escaping (AnyObject) -> Void) {
+        var urlrequest = URLRequest(url: url)
+        urlrequest.httpMethod = "PUT"
         
+        let jsonObject: Data
+        
+        do {
+            jsonObject = try JSONSerialization.data(withJSONObject: object, options: [])
+            urlrequest.httpBody = jsonObject
+        } catch {
+            print("\(logTag) Error, cannot produce JSON from object")
+            return
+        }
+        
+        let task = session.dataTask(with: urlrequest) {
+            (data, response, error) -> Void in
+            
+            do {
+                
+                // Check for any errors
+                guard error == nil else {
+                    print("\(logTag) Error with PUT request")
+                    print(error!)
+                    return
+                }
+                
+                // Check the response data
+                guard let responseData = data else {
+                    print("\(logTag) Error, did not receive data")
+                    return
+                }
+                
+                print(responseData)
+                
+                // Serialize with JSON
+                let json: AnyObject? = try JSONSerialization.jsonObject(
+                    with: responseData, options: .mutableContainers) as AnyObject
+                
+                if let j: AnyObject = json {
+                    
+                    OperationQueue.main.addOperation {
+                        completion(j)
+                    }
+                    
+                } else {
+                    print("\(logTag) Error, Map Conversion Failed")
+                    return
+                }
+                
+            } catch let error {
+                print(error)
+            }
+        }
+        task.resume()
     }
     
     // Handle HTTP DELETE Requests to the API
-    public static func delete(withURL url: String) {
+    public static func delete(withURL url: URL, andObject object: Any,
+                              completion: @escaping (AnyObject) -> Void) {
+        var urlrequest = URLRequest(url: url)
+        urlrequest.httpMethod = "DELETE"
         
+        let task = session.dataTask(with: urlrequest) {
+            (data, response, error) -> Void in
+            
+            guard let _ = data else {
+                print("\(logTag) Error, Delete Request failed")
+                return
+            }
+            print("\(logTag) Delete Request Successful!")
+        }
+        task.resume()
     }
 }
