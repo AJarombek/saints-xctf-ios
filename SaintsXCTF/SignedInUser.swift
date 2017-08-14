9bb0f014a50fdc4d9680eb11e56af612654ee5f2
Andrew Jarombek <ajarombek95@gmail.com>
2017-08-14 00:02:45 -0400

    V.0.0.28 - Work on Archiving Signed In User

---
+++ SaintsXCTF/SignedInUser.swift
@@ -0,0 +1,37 @@
+//
+//  SignedInUser.swift
+//  SaintsXCTF
+//
+//  Created by Andy Jarombek on 8/13/17.
+//  Copyright © 2017 Andy Jarombek. All rights reserved.
+//
+
+import UIKit
+import os.log
+
+class SignedInUser {
+    
+    let logTag = OSLog(subsystem: "SaintsXCTF.App.SignedInUser", category: "SignedInUser")
+    
+    var user = User()
+    
+    // Get the url to the document directory where we will store the user object
+    let userArchiveURL: URL = {
+        let documentsDirectories = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        let documentDirectory = documentsDirectories.first!
+        return documentDirectory.appendingPathComponent("user.archive")
+    }()
+    
+    // On initialization, check to see if an archived user exists
+    init() {
+        if let archivedUser = NSKeyedUnarchiver.unarchiveObject(withFile: userArchiveURL.path) as? User {
+            user = archivedUser
+        }
+    }
+    
+    // Save the user to the filesystem
+    func saveUser() -> Bool {
+        os_log("Saving items to %@", log: OSLog.default, type: .debug, userArchiveURL.path)
+        return NSKeyedArchiver.archiveRootObject(user, toFile: userArchiveURL.path)
+    }
+}
--- SaintsXCTF/User.swift
+++ SaintsXCTF/User.swift
@@ -9,7 +9,7 @@
 import Foundation
 import ObjectMapper
 
-class User: Mappable, CustomStringConvertible {
+class User: NSObject, Mappable, NSCoding {
     
     var username: String!
     var first: String!
@@ -33,18 +33,69 @@ class User: Mappable, CustomStringConvertible {
     var last_signin: String?
     var week_start: String?
     
-    init() {
+    override init() {
         username = ""
     }
     
-    required init?(map: Map) {
-        // pass
+    // NSCoder Initializer
+    // A required initializer means that all subclasses must implement the initializer
+    required init?(coder aDecoder: NSCoder) {
+        username = aDecoder.decodeObject(forKey: "username") as! String!
+        first = aDecoder.decodeObject(forKey: "first") as! String!
+        last = aDecoder.decodeObject(forKey: "last") as! String!
+        email = aDecoder.decodeObject(forKey: "email") as! String?
+        salt = aDecoder.decodeObject(forKey: "salt") as! String?
+        password = aDecoder.decodeObject(forKey: "password") as! String?
+        profilepic = aDecoder.decodeObject(forKey: "profilepic") as! String?
+        profilepic_name = aDecoder.decodeObject(forKey: "profilepic_name") as! String?
+        user_description = aDecoder.decodeObject(forKey: "description") as! String?
+        activation_code = aDecoder.decodeObject(forKey: "activation_code") as! String?
+        member_since = aDecoder.decodeObject(forKey: "member_since") as! Date!
+        class_year = aDecoder.decodeInteger(forKey: "class_year")
+        location = aDecoder.decodeObject(forKey: "location") as! String?
+        favorite_event = aDecoder.decodeObject(forKey: "favorite_event") as! String?
+        forgot_password = aDecoder.decodeObject(forKey: "forgot_password") as! [String]!
+        flair = aDecoder.decodeObject(forKey: "flair") as! [String]!
+        notifications = aDecoder.decodeObject(forKey: "notifications") as! [Notification]!
+        groups = aDecoder.decodeObject(forKey: "groups") as! [GroupInfo]!
+        statistics = aDecoder.decodeObject(forKey: "statistics") as! [String:Double]!
+        last_signin = aDecoder.decodeObject(forKey: "last_signin") as! String?
+        week_start = aDecoder.decodeObject(forKey: "week_start") as! String?
+        
+        super.init()
     }
+    
+    // Mappable Initializer
+    required init?(map: Map) {}
     
     // This class uses the CustomStringConvertible protocol.
     // The description will be printed whenever we try to print a User object
-    var description: String {
+    override var description: String {
         return "User: (\(username!), \(first!), \(last!))"
+    }
+    
+    func encode(with aCoder: NSCoder) {
+        aCoder.encode(username, forKey: "username")
+        aCoder.encode(first, forKey: "first")
+        aCoder.encode(last, forKey: "last")
+        aCoder.encode(email, forKey: "email")
+        aCoder.encode(salt, forKey: "salt")
+        aCoder.encode(password, forKey: "password")
+        aCoder.encode(profilepic, forKey: "profilepic")
+        aCoder.encode(profilepic_name, forKey: "profilepic_name")
+        aCoder.encode(user_description, forKey: "description")
+        aCoder.encode(activation_code, forKey: "activation_code")
+        aCoder.encode(member_since, forKey: "member_since")
+        aCoder.encode(class_year, forKey: "class_year")
+        aCoder.encode(location, forKey: "location")
+        aCoder.encode(favorite_event, forKey: "favorite_event")
+        aCoder.encode(forgot_password, forKey: "forgot_password")
+        aCoder.encode(flair, forKey: "flair")
+        aCoder.encode(notifications, forKey: "notifications")
+        aCoder.encode(groups, forKey: "groups")
+        aCoder.encode(statistics, forKey: "statistics")
+        aCoder.encode(last_signin, forKey: "last_signin")
+        aCoder.encode(week_start, forKey: "week_start")
     }
     
     func mapping(map: Map) {
