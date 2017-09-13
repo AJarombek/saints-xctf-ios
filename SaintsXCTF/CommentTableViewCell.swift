f9fc29f9f5b1234ddd094980e4551fadd36491d1
Andrew Jarombek <ajarombek95@gmail.com>
2017-09-12 22:42:31 -0400

    V.0.0.48 - CommentView Formatted

--- SaintsXCTF/CommentTableViewCell.swift
+++ SaintsXCTF/CommentTableViewCell.swift
@@ -14,4 +14,29 @@ class CommentTableViewCell: UITableViewCell {
     @IBOutlet weak var dateLabel: UILabel!
     @IBOutlet weak var contentLabel: UILabel!
     
+    // Add padding to the cell
+    override var frame: CGRect {
+        get {
+            return super.frame
+        }
+        set {
+            var frame = newValue
+            frame.origin.x += 2
+            frame.size.width -= 4
+            
+            frame.origin.y += 2
+            frame.size.height -= 4
+            
+            super.frame = frame
+        }
+    }
+    
+    // Called when the cell is about to be reused.  Reset all of the text fields
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        
+        contentLabel.text = ""
+        dateLabel.text = ""
+        nameLabel.text = ""
+    }
 }
--- SaintsXCTF/CommentViewController.swift
+++ SaintsXCTF/CommentViewController.swift
@@ -17,16 +17,10 @@ class CommentViewController: UITableViewController {
     
     var log: Log!
     var comments: [Comment]?
+    let heightDict = NSMutableDictionary()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // Make sure the table view does not overlap the status bar
-        let statusBarHight = UIApplication.shared.statusBarFrame.height
-        let insets = UIEdgeInsets(top: statusBarHight, left: 0, bottom: 0, right: 0)
-        
-        commentTableView.contentInset = insets
-        commentTableView.scrollIndicatorInsets = insets
         
         // Setting the row height to UITableViewAutomaticDimension tells the TableView to determine the
         // height of a cell based on its contents and constraints.
@@ -35,6 +29,12 @@ class CommentViewController: UITableViewController {
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+        
+        // Set the navigation bar back button to a custom image
+        navigationController?.navigationBar.backIndicatorImage = UIImage(named: "back")
+        navigationController?.navigationBar.backIndicatorTransitionMaskImage = UIImage(named: "back")
+        navigationController?.navigationBar.tintColor = UIColor(0x000000)
+        navigationItem.backBarButtonItem = UIBarButtonItem(title: " ", style: UIBarButtonItemStyle.plain, target: nil, action: nil)
         
         os_log("CommentViewController Appeared.", log: logTag, type: .debug)
         
@@ -49,6 +49,24 @@ class CommentViewController: UITableViewController {
             return commentcount + 1
         } else {
             return 1
+        }
+    }
+    
+    // Called when a cell at a certain index is about to be displayed
+    override func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell,
+                            forRowAt indexPath: IndexPath) {
+        
+        // Cache the height of the cell
+        let height = NSNumber(value: Float(cell.frame.size.height))
+        heightDict.setObject(height, forKey: indexPath as NSCopying)
+    }
+    
+    // Either set the height to the cached value or let the automaticdimension determine the height
+    override func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
+        if let height = heightDict.object(forKey: indexPath) as? NSNumber {
+            return CGFloat(height.floatValue)
+        } else {
+            return UITableViewAutomaticDimension
         }
     }
     
@@ -70,21 +88,25 @@ class CommentViewController: UITableViewController {
                     as! CommentTableViewCell
         
             if let comment = comments?[indexPath.row - 1] {
+                
+                cell.layer.borderWidth = 2
+                cell.layer.borderColor = UIColor(0xAAAAAA).cgColor
+                cell.layer.cornerRadius = 1
+                cell.backgroundColor = UIColor(Constants.getFeelColor(6))
             
-                /*
-                 // Convert the string to a date
-                 let dateFormatter = DateFormatter()
-                 dateFormatter.dateFormat = "yyyy-MM-dd HH:mm"
+                // Convert the string to a date
+                let dateFormatter = DateFormatter()
+                dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
                  
-                 // Then format the date for viewing
-                 let formattedDate = dateFormatter.date(from: log.date)
-                 dateFormatter.dateFormat = "MMM dd, yyyy"
-                 cell.dateLabel?.text = dateFormatter.string(from: formattedDate!)
-                 
-                 cell.nameLabel?.text = log.name!*/
+                // Then format the date for viewing
+                let formattedDate = dateFormatter.date(from: comment.time)
+                dateFormatter.amSymbol = "AM"
+                dateFormatter.pmSymbol = "PM"
+                dateFormatter.dateFormat = "MMM dd, yyyy h:mm a"
+                cell.dateLabel.text = dateFormatter.string(from: formattedDate!)
             
                 cell.contentLabel.text = comment.content!
-                cell.dateLabel.text = comment.time!
+                // cell.dateLabel.text = comment.time!
                 cell.nameLabel.text = "\(comment.first!) \(comment.last!)"
             
             } else {
@@ -93,9 +115,5 @@ class CommentViewController: UITableViewController {
         
             return cell
         }
-    }
-    
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        
     }
 }
