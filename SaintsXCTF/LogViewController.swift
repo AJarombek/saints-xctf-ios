adb9ade53b6fb8f82399b865f26c52d226bf156e
Andrew Jarombek <ajarombek95@gmail.com>
2017-09-16 22:34:15 -0400

    V.0.0.56 - Begin Work on Log Input

--- SaintsXCTF/LogViewController.swift
+++ SaintsXCTF/LogViewController.swift
@@ -11,6 +11,27 @@ import os.log
 
 class LogViewController: UIViewController {
     
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var locationField: UITextField!
+    @IBOutlet weak var dateField: UITextField!
+    @IBOutlet weak var typeField: UITextField!
+    @IBOutlet weak var distanceField: UITextField!
+    @IBOutlet weak var metricField: UITextField!
+    @IBOutlet weak var minutesField: UITextField!
+    @IBOutlet weak var secondsField: UITextField!
+    @IBOutlet weak var feelStepper: UIStepper!
+    @IBOutlet weak var descriptionField: UITextView!
+    @IBOutlet weak var errorField: UITextView!
+    @IBOutlet weak var submitButton: UIButton!
+    @IBOutlet weak var cancelButton: UIButton!
+    
+    let distanceRegex = "^[0-9]{0,3}(\\.[0-9]{1,2})?$"
+    let minuteRegex = "^[0-9]{1,5}$"
+    let secondRegex = "^[0-9]{1,2}$"
+    let userTagRegex = "@[a-zA-Z0-9]+"
+    
+    let currentDate = "08/16/2017"
+    
     let logTag = OSLog(subsystem: "SaintsXCTF.App.LogViewController", category: "LogViewController")
     
     override func viewDidLoad() {
@@ -21,5 +42,23 @@ class LogViewController: UIViewController {
     // Remove the keyboard when tapping the background
     @IBAction func backgroundTapped(_ sender: UITapGestureRecognizer) {
         view.endEditing(true)
+    }
+    
+    @IBAction func submitLog(_ sender: UIButton) {
+        
+    }
+    
+    // When the user clicks cancel, reset all the fields to their default values
+    @IBAction func cancelLog(_ sender: UIButton) {
+        nameField.text = ""
+        locationField.text = ""
+        dateField.text = ""
+        typeField.text = "Run"
+        distanceField.text = ""
+        metricField.text = "Miles"
+        minutesField.text = ""
+        secondsField.text = ""
+        descriptionField.text = ""
+        errorField.text = ""
     }
 }
