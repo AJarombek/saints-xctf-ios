78dd68ebb76cb28b7dad432a3a660e7cdfefe47c
Andrew Jarombek <ajarombek95@gmail.com>
2017-10-07 17:54:53 -0400

    V.0.0.91 - Weekly Bar Chart Populates

--- SaintsXCTF/WeeklyViewController.swift
+++ SaintsXCTF/WeeklyViewController.swift
@@ -20,14 +20,19 @@ class WeeklyViewController: UIViewController {
     @IBOutlet weak var swimFilterButton: UIButton!
     @IBOutlet weak var otherFilterButton: UIButton!
     
+    @IBOutlet weak var barChartView: BarChartView!
+    
     var user: User = User()
     var weekstart: String = ""
     var weekstartOffset: Int = 0
     var run = true, bike = false, swim = false, other = false
-    var weekTotals: [Double] = [0,0,0,0,0,0,0,0,0,0]
     
+    var startDate: Date = Date()
+    var endDate: Date = Date()
     var start = ""
     var end = ""
+    
+    var chartLabels: [String] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -54,31 +59,6 @@ class WeeklyViewController: UIViewController {
         
         // By default runs are shown in the weekly graph
         filterRun()
-        
-        // Interval to add a day to a date
-        let day: TimeInterval = 60 * 60 * 24
-        
-        // Initialize the date range for the graph
-        let calendar = Calendar(identifier: .gregorian)
-        
-        // Get the day of the week
-        let date: Date = Date()
-        var weekdayComponent = calendar.dateComponents([.weekday], from: date)
-        let weekday = weekdayComponent.weekday!
-        
-        // Set the end date as the end of this week and start date as 10 weeks previous
-        var endDate: Date = Calendar.current.date(byAdding: .day, value: weekstartOffset - weekday, to: date)!
-        endDate.addTimeInterval(day * 7)
-        var startDate: Date = endDate
-        startDate.addTimeInterval(day * -69)
-        
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "yyyy-MM-dd"
-        
-        end = dateFormatter.string(from: endDate)
-        start = dateFormatter.string(from: startDate)
-        print(start)
-        print(end)
         
         // Initialize the graph itself
         initialize()
@@ -161,12 +141,122 @@ class WeeklyViewController: UIViewController {
     // Function to Initialize the creation of the weekly graph
     func initialize() {
         
+        // Interval to add a day to a date
+        let day: TimeInterval = 60 * 60 * 24
+        
+        // Initialize the date range for the graph
+        let calendar = Calendar(identifier: .gregorian)
+        
+        // Get the day of the week
+        let date: Date = Date()
+        var weekdayComponent = calendar.dateComponents([.weekday], from: date)
+        let weekday = weekdayComponent.weekday!
+        
+        // Set the end date as the end of this week and start date as 10 weeks previous
+        endDate = Calendar.current.date(byAdding: .day, value: weekstartOffset - weekday, to: date)!
+        endDate.addTimeInterval(day * 7)
+        startDate = endDate
+        startDate.addTimeInterval(day * -69)
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd"
+        
+        end = dateFormatter.string(from: endDate)
+        start = dateFormatter.string(from: startDate)
+        print(start)
+        print(end)
+        
+        var weekDates = startDate
+        
+        dateFormatter.dateFormat = "MMM. dd"
+        
+        // Set the labels for the chart as the start date of each week
+        for _ in 0...9 {
+            chartLabels.append(dateFormatter.string(from: weekDates))
+            weekDates.addTimeInterval(day * 7)
+        }
+        
+        // Set up some default chart values
+        barChartView.backgroundColor = UIColor(0xFFFFFF)
+        barChartView.chartDescription?.text = ""
+        
+        // Setup the axis
+        barChartView.xAxis.labelPosition = .bottom
+        barChartView.xAxis.drawGridLinesEnabled = false
     }
     
     // Function to populate the weekly graph based on the filter
     func populate() {
         
         let typeFilter = getTypeFilter()
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd"
+        
+        // Get the range view from the API for this month and user
+        APIClient.rangeViewGetRequest(withParamType: "user", sortParam: user.username!, filter: typeFilter, start: start, end: end) {
+            rangeView -> Void in
+            
+            var logsWeekly: [Int] = [0,0,0,0,0,0,0,0,0,0]
+            var mileageWeekly: [Double] = [0,0,0,0,0,0,0,0,0,0]
+            var totalfeelWeekly: [Int] = [0,0,0,0,0,0,0,0,0,0]
+            var feelWeekly: [Int] = [0,0,0,0,0,0,0,0,0,0]
+            
+            // Look at each day in the range view and populate the mileage and feel for each week
+            rangeView.forEach {
+                activity -> Void in
+                
+                let mileage: Double = Double(activity.miles)!
+                let feel: Int = Int(activity.feel)!
+                let dateString: String = activity.date!
+                let date: Date = dateFormatter.date(from: dateString)!
+                
+                let difComponent = Calendar.current.dateComponents([.day], from: self.startDate, to: date)
+                let day = difComponent.day!
+                
+                let weekIndex: Int = day / 7
+                logsWeekly[weekIndex] += 1
+                mileageWeekly[weekIndex] += mileage
+                totalfeelWeekly[weekIndex] += feel
+            }
+            
+            print(logsWeekly)
+            print(mileageWeekly)
+            print(totalfeelWeekly)
+            
+            // Populate feelWeekly with the average feel for each week
+            for i in 0...9 {
+                let logCount = logsWeekly[i]
+                
+                if logCount != 0 {
+                    feelWeekly[i] = totalfeelWeekly[i] / logCount
+                } else {
+                    feelWeekly[i] = 0
+                }
+                
+            }
+            
+            // Finally set up the bar chart with the given data
+            self.setBarChart(dataPoints: self.chartLabels, mileage: mileageWeekly, feel: feelWeekly)
+        }
+    }
+    
+    // Set up the bar chart with the data points, mileage and feel
+    func setBarChart(dataPoints: [String], mileage: [Double], feel: [Int]) {
+        var dataEntries: [BarChartDataEntry] = []
+        
+        for i in 0..<self.chartLabels.count {
+            let chartEntry = BarChartDataEntry(x: Double(i), y: mileage[i])
+            dataEntries.append(chartEntry)
+        }
+        
+        let chartDataSet = BarChartDataSet(values: dataEntries, label: "Week")
+        let chartData = BarChartData()
+        chartData.addDataSet(chartDataSet)
+        chartData.setDrawValues(true)
+        chartDataSet.colors = [UIColor(0xCCCCCC)]
+        
+        barChartView.data = chartData
     }
     
     // Return the type filter to send to the rangeview API endpoint
