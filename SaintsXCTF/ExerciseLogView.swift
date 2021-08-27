c3832cf2970551bbf933d329afc15fd4c5c85e10
ajarombek <ajarombek95@gmail.com>
2021-08-26 21:21:39 -0400

    more work on exercise log swift ui view

--- SaintsXCTF/ExerciseLogView.swift
+++ SaintsXCTF/ExerciseLogView.swift
@@ -9,21 +9,100 @@
 import SwiftUI
 
 struct ExerciseLogView: View {
+    @State private var name: String = ""
+    @State private var isEditingName: Bool = false
+    
     var body: some View {
         VStack(alignment: .leading) {
             Text("Create Exercise Log")
-                .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                .font(.title)
                 .foregroundColor(.black)
-            VStack {
-                Text("Exercise Name*")
-                    .font(.subheadline)
+                .bold()
+            VStack(alignment: .leading) {
                 HStack {
-                    Text("Location")
+                    Text("Average")
                         .font(.subheadline)
-                    Spacer()
-                    Text("Date*")
-                        .font(.subheadline)
+                        .foregroundColor(Color(UIColor(0x737373)))
                 }
+                .frame(minWidth: 0, maxWidth: .infinity, alignment: .trailing)
+                VStack(alignment: .leading) {
+                    Text("Exercise Name*")
+                        .font(.subheadline)
+                        .foregroundColor(Color(UIColor(0xA96A5B)))
+                        .bold()
+                    TextField("", text: $name) { isEditing in
+                        self.isEditingName = isEditing
+                    }
+                    .autocapitalization(.none)
+                    .disableAutocorrection(true)
+                    .background(Color(UIColor.white))
+                    .frame(minHeight: 30)
+                }
+                .padding(.top, 5.0)
+                HStack {
+                    VStack {
+                        Text("Location")
+                            .font(.subheadline)
+                            .foregroundColor(Color(UIColor(0xA96A5B)))
+                            .bold()
+                    }
+                    VStack {
+                        Text("Date*")
+                            .font(.subheadline)
+                            .foregroundColor(Color(UIColor(0xA96A5B)))
+                            .bold()
+                    }
+                }
+                .padding(.top, 5.0)
+                .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+                HStack {
+                    Text("Exercise Type")
+                        .font(.subheadline)
+                        .foregroundColor(Color(UIColor(0xA96A5B)))
+                        .bold()
+                }
+                .padding(.top, 5.0)
+                HStack {
+                    Text("Distance")
+                        .font(.subheadline)
+                        .foregroundColor(Color(UIColor(0xA96A5B)))
+                        .bold()
+                    Text("Time")
+                        .font(.subheadline)
+                        .foregroundColor(Color(UIColor(0xA96A5B)))
+                        .bold()
+                }
+                .padding(.top, 5.0)
+                HStack {
+                    Text("Feel")
+                        .font(.subheadline)
+                        .foregroundColor(Color(UIColor(0xA96A5B)))
+                        .bold()
+                }
+                .padding(.top, 5.0)
+                HStack {
+                    Text("Description")
+                        .font(.subheadline)
+                        .foregroundColor(Color(UIColor(0xA96A5B)))
+                        .bold()
+                }
+                .padding(.top, 5.0)
+                HStack(spacing: 20) {
+                    Button(action: {
+                        print("Create")
+                    }) {
+                        Text("Create")
+                    }
+                    Button(action: {
+                        print("Cancel")
+                    }) {
+                        Text("Cancel")
+                            .foregroundColor(Color(UIColor(0x990000)))
+                    }
+                }
+                .padding(.top, 15)
+                .padding(.trailing, 10)
+                .frame(minWidth: 0, maxWidth: .infinity, alignment: .trailing)
             }
             .padding()
             .overlay(
@@ -32,6 +111,7 @@ struct ExerciseLogView: View {
                     .shadow(radius: 2)
             )
             .background(Color.init(UIColor(0xF8F8F8)))
+            .textFieldStyle(RoundedBorderTextFieldStyle())
         }
         .padding()
     }
